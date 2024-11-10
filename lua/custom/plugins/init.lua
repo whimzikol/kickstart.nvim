96d84f4aa24a0ddfb3d16765ea47a4e3410428a8
whimzikol <chris.santos@gmail.com>
2024-11-10 23:29:06 +0100

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+  "Diogo-ss/42-header.nvim",
+  cmd = { "Stdheader" },
+  keys = { "<F2>" },
+  opts = {
+    default_map = false, -- Default mapping <F1> in normal mode.
+    auto_update = true, -- Update header when saving.
+    user = "username", -- Your user.
+    mail = "username@student.42berlin.de", -- Your mail.
+    -- add other options.
+  },
+  config = function(_, opts)
+    require("42header").setup(opts)
+  end,
+}
+}
